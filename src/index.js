89302396c9ce5eacddd626c9b7d632ac60c8ac77
Gabriel Augusto <gabriel.augusto@grupoportfolio.com.br>
2023-05-02 00:04:42 -0300

    Add new configuration

--- src/index.js
+++ src/index.js
@@ -5,30 +5,34 @@
 //https://github.com/ZijianHe/koa-router
 
 // todas as configuraçoes devem ser passadas via environment variables
+const app = require('./app')
+const dotenv = require('dotenv')
+
+dotenv.config();
+
 const PORT = process.env.PORT || 3000;
 
-const Koa = require('koa');
-const Router = require('koa-router');
+app.listen(PORT, () => {
+  console.log(`server runing at http://localhost:${PORT}`)
+  console.log(`access the swagger in the following link http://localhost:${PORT}/api`)
+})
 
-const koa = new Koa();
-var router = new Router();
+// //rota simples pra testar se o servidor está online
+// router.get('/', async (ctx) => {
+//   ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
+// });
 
-//rota simples pra testar se o servidor está online
-router.get('/', async (ctx) => {
-  ctx.body = `Seu servidor esta rodando em http://localhost:${PORT}`; //http://localhost:3000/
-});
+// //Uma rota de exemplo simples aqui.
+// //As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
+// router.get('/users', async (ctx) => {
+//     ctx.status = 200;
+//     ctx.body = {total:0, count: 0, rows:[]}
+// });
 
-//Uma rota de exemplo simples aqui.
-//As rotas devem ficar em arquivos separados, /src/controllers/userController.js por exemplo
-router.get('/users', async (ctx) => {
-    ctx.status = 200;
-    ctx.body = {total:0, count: 0, rows:[]}
-});
+// koa
+//   .use(router.routes())
+//   .use(router.allowedMethods());
 
-koa
-  .use(router.routes())
-  .use(router.allowedMethods());
+// const server = koa.listen(PORT);
 
-const server = koa.listen(PORT);
-
-module.exports = server;
+// module.exports = server;
